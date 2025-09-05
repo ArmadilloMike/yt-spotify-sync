f29c0e6078c33999c60a0da3beb987ccaef7e003
JacksonWengler <jacksonwengler@gmail.com>
2025-09-05 17:07:47 -0500

    Renamed the Spotify variables and authenticate function to have Spotify in front of it to differentiate from the soon-to-be added google versions

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import os
 import urllib.parse
 import webbrowser
 from http.server import BaseHTTPRequestHandler, HTTPServer
-
 import requests
 from dotenv import load_dotenv
+from google_auth_oauthlib.flow import InstalledAppFlow
 
 load_dotenv()
-CLIENT_ID = os.getenv("CLIENT_ID")
-CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-REDIRECT_URI = os.getenv("REDIRECT_URI")
-SCOPE = os.getenv("SCOPE")
+SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+SPOTIFY_REDIRECT_URI = os.getenv("SPOTIFY_REDIRECT_URI")
+SPOTIFY_SCOPE = os.getenv("SPOTIFY_SCOPE")
 
 class CallbackHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -22,9 +22,9 @@ class CallbackHandler(BaseHTTPRequestHandler):
             payload = {
                 "grant_type": "authorization_code",
                 "code": code,
-                "redirect_uri": REDIRECT_URI,
-                "client_id": CLIENT_ID,
-                "client_secret": CLIENT_SECRET
+                "redirect_uri": SPOTIFY_REDIRECT_URI,
+                "client_id": SPOTIFY_CLIENT_ID,
+                "client_secret": SPOTIFY_CLIENT_SECRET
             }
             responce = requests.post(token_url, data=payload)
             self.send_response(200)
@@ -37,17 +37,17 @@ class CallbackHandler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(b"Missing code parameter.")
 
-def authenticate():
+def spotify_authenticate():
     auth_url = (
         "https://accounts.spotify.com/authorize"
-        f"?response_type=code&client_id={CLIENT_ID}"
-        f"&scope={SCOPE}&redirect_uri={REDIRECT_URI}"
+        f"?response_type=code&client_id={SPOTIFY_CLIENT_ID}"
+        f"&scope={SPOTIFY_SCOPE}&redirect_uri={SPOTIFY_REDIRECT_URI}"
     )
     webbrowser.open(auth_url)
-    port = int(urllib.parse.urlparse(REDIRECT_URI).port)
+    port = int(urllib.parse.urlparse(SPOTIFY_REDIRECT_URI).port)
     server = HTTPServer(("localhost", port), CallbackHandler)
     print("waiting for spotify auth")
     server.handle_request()
 
 if __name__ == "__main__":
-    authenticate()
+    spotify_authenticate()
